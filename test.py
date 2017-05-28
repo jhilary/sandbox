89f42e99d74f2624077cf1e6aec5d14b828c2f69
ilariia <ilariia@yandex-team.ru>
2017-05-28 20:43:40 +0300

    Fix render, remove unused bots

--- test.py
+++ test.py
@@ -10,78 +10,6 @@ from utils import Inteleaving
 from bots import *
 
 
-class BaselinePlayer(Player):
-    def __init__(self, name):
-        self._name = name
-        self._my_card = None
-
-    def say_card(self) -> Card:
-        return random.choice([Card.RED, Card.BLACK])
-
-    def opponent_said_card(self, opponents_bid: Card) -> None:
-        return None
-
-    def would_change_card(self) -> bool:
-        return random.choice([True, False])
-
-    def opponent_change_card(self, is_changed: bool) -> None:
-        pass
-
-    def win(self, value):
-        pass
-
-    def end_round(self):
-        pass
-
-    def take_card(self, card: Card) -> None:
-        self._my_card = card
-
-    def opponent_card(self, card: Card) -> None:
-        pass
-
-    @property
-    def name(self) -> str:
-        return self._name
-
-
-class SmarterBaseline(Player):
-    def __init__(self, name):
-        self._name = name
-        self._my_card = None
-        self._opponent_card = None
-
-    @property
-    def name(self) -> str:
-        return self._name
-
-    def take_card(self, card: Card) -> None:
-        self._my_card = card
-
-    def say_card(self) -> Card:
-        if self._my_card == Card.RED:
-            return Card.BLACK
-        else:
-            return Card.RED
-
-    def opponent_said_card(self, opponents_bid: Card) -> None:
-        self._opponent_card = opponents_bid
-
-    def would_change_card(self) -> bool:
-        return False
-
-    def opponent_change_card(self, is_changed: bool) -> None:
-        self._opponent_card = Card.BLACK if self._opponent_card == Card.RED else Card.RED
-
-    def win(self, value: int) -> None:
-        pass
-
-    def opponent_card(self, card: Card) -> None:
-        pass
-
-    def end_round(self) -> None:
-        pass
-
-
 def versus(p1, p2):
     winners: Dict[Player, int] = {p1: 0, p2: 0}
     games = 1000
@@ -129,9 +57,9 @@ def main():
 
     opponent = SmarterBaselineBot()
     env = CardsGuessing(starting_money=100, opponent=opponent)
-    player = MishaBotV1New(debug=False)
+    player = IlariiaRandomUltimatumBot(100, debug=True)
     player.set_env(env)
-    player.run(100)
+    player.run(1)
 
 if __name__ == "__main__":
     main()
